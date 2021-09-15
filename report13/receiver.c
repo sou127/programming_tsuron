e43d507c92c3c2b8c96a3b17b3b4a4a75a600a7d
sou127 <srbh453@gmail.com>
2021-09-16 00:20:34 +0900

    report 13

---
+++ report13/receiver.c
@@ -0,0 +1,140 @@
+//server as receiver
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<arpa/inet.h>   
+#include<unistd.h> 
+#include<errno.h>
+
+int receive_image(int socket){ // Start function 
+    int buffersize = 0, recv_size = 0,size = 0, read_size, write_size, packet_index =1,stat;
+    char filename[1024]; char buffer[] = "Got it";
+    char imagearray[10241],verify = '1';
+    FILE *image;
+
+
+    read(socket, filename, sizeof(char *));
+
+    //Find the size of the image
+    do{
+        stat = read(socket, &size, sizeof(int));
+    }while(stat<0);
+
+
+    printf("Packets received.\n");
+    printf("Image size: %i\n",size);
+
+    //Send our verification signal
+    do{
+        stat = write(socket, &buffer, sizeof(int));
+    }while(stat<0);
+
+    image = fopen(filename, "w");
+
+    if( image == NULL) {
+        printf("Error has occurred. Image file could not be opened\n");
+        return -1; 
+    }
+
+    int need_exit = 0;
+    struct timeval timeout = {10,0};
+
+    fd_set fds;
+    int buffer_fd, buffer_out;
+
+    while(recv_size < size) {
+    //while(packet_index < 2){
+
+        FD_ZERO(&fds);
+        FD_SET(socket,&fds);
+
+        buffer_fd = select(FD_SETSIZE,&fds,NULL,NULL,&timeout);
+
+        if (buffer_fd < 0)
+           printf("error: bad file descriptor set.\n");
+
+        if (buffer_fd == 0)
+           printf("error: buffer read timeout expired.\n");
+
+        if (buffer_fd > 0)
+        {
+            do{
+                read_size = read(socket,imagearray, 10241);
+            }while(read_size <0);
+
+            //Write the currently read data into our image file
+            write_size = fwrite(imagearray,1,read_size, image);
+
+            if(read_size !=write_size) {
+                printf("error in read write\n");    
+            }
+
+
+            recv_size += read_size;
+            packet_index++;
+        }
+
+    }
+
+    fclose(image);
+    printf("Image successfully Received! as %s\n", filename);
+    return 1;
+}
+
+
+int main(int argc , char *argv[]) {
+  if(argc != 2){
+    printf("usage: \n\t %s 8080[Port Number]\n",argv[0]);
+    exit(0);
+  }
+  else{
+    int PORT = atoi(argv[1]);
+    int socket_desc , new_socket , c, read_size,buffer = 0;
+    struct sockaddr_in server , client;
+    char *readin;
+
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1){
+      printf("Could not create socket");
+    }
+
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( PORT );
+
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0){
+     puts("bind failed");
+     return 1;
+    }
+
+    puts("bind done");
+
+    //Listen
+    listen(socket_desc , 3);
+
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+
+    if((new_socket = accept(socket_desc, (struct sockaddr *)&client,(socklen_t*)&c))){
+      puts("Connection accepted");
+    }
+
+    fflush(stdout);
+
+    if (new_socket<0){
+      perror("Accept Failed");
+      return 1;
+    }
+
+    receive_image(new_socket);
+    close(socket_desc);
+    fflush(stdout);
+    return 0;
+  }
+}
---
+++ report13/sender.c
@@ -0,0 +1,102 @@
+//client as sender
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>   
+#include<sys/ioctl.h>
+#include<stdlib.h>
+#include<unistd.h> 
+#include<errno.h>
+
+int send_image(int socket, char* local_file, char* remote_file){
+  FILE *picture;
+  int size, read_size, stat;
+  char send_buffer[10240], read_buffer[256];
+
+  picture = fopen(local_file, "r");
+  printf("Getting Picture Size\n");   
+
+  if(picture == NULL) {
+    printf("Error Opening Image File"); 
+  } 
+
+  fseek(picture, 0, SEEK_END);
+  size = ftell(picture);
+  fseek(picture, 0, SEEK_SET);
+  printf("Total Picture size: %i\n",size);
+
+  //Send remote file name
+  printf("Sending Remote File Name\n");
+  write(socket, (void *)remote_file, sizeof(remote_file));
+
+  //Send Picture Size
+  printf("Sending Picture Size\n");
+  write(socket, (void *)&size, sizeof(int));
+
+  //Send Picture as Byte Array
+  printf("Sending Picture as Byte Array\n");
+
+  do { //Read while we get errors that are due to signals.
+    stat=read(socket, &read_buffer , 255);
+  } while (stat < 0);
+
+  printf("Received data in socket\n");
+  printf("Socket data: %s\n", read_buffer);
+
+  while(!feof(picture)) {
+    //Read from the file into our send buffer
+    read_size = fread(send_buffer, 1, sizeof(send_buffer)-1, picture);
+
+    //Send data through our socket 
+    do{
+      stat = write(socket, send_buffer, read_size);  
+    }while (stat < 0);
+
+    //Zero out our send buffer
+
+    bzero(send_buffer, sizeof(send_buffer));
+  }
+}
+
+int main(int argc , char *argv[]) {
+    if(argc != 5){
+        printf("sample usage: \n\t %s 127.0.0.1 8080 1_local.jpg 1_remote.jpg\n",argv[0]);
+        exit(0);
+    }
+    else{
+        char* address = argv[1];
+        int PORT = atoi(argv[2]);
+        char* local_file = argv[3];
+        char* remote_file = argv[4];
+
+        int socket_desc;
+        struct sockaddr_in server;
+        char *parray;
+
+        //Create socket
+        socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+
+        if (socket_desc == -1) {
+            printf("Could not create socket");
+        }
+
+        memset(&server,0,sizeof(server));
+        server.sin_addr.s_addr = inet_addr( address );
+        server.sin_family = AF_INET;
+        server.sin_port = htons( PORT );
+
+        //Connect to remote server
+        if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0) {
+            printf("%s",strerror(errno));
+            close(socket_desc);
+            puts("Connect Error");
+            return 1;
+        }
+        puts("Connected\n");
+        
+        send_image(socket_desc, local_file, remote_file);
+        close(socket_desc);
+
+        return 0;
+    }
+  }
